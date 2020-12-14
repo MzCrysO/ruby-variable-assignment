5446a4a07b5dbf553f41773eeccf4bb022687a25
MzCrysO <+>
2020-12-14 00:50:03 -0600

    commit tree b515f41397fc18b223f807f8a057dd6c5a864b22

--- variable.rb
+++ variable.rb
@@ -1,2 +1,7 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+ 
+greeting = 'Hello World! Never Fear CzCrysO is here!'
+puts greeting
+
+greeting = 'Hello World'
